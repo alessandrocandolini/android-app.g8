a7ad219b8383d6229eba8a1f68990e68289877aa
Alessandro Candolini <alessandrocandolini@gmail.com>
2023-10-01 04:19:54 +0100

    more toml

--- src/main/g8/settings.gradle.kts
+++ src/main/g8/settings.gradle.kts
@@ -3,6 +3,9 @@ pluginManagement {
         google()
         mavenCentral()
         gradlePluginPortal()
+        maven {
+           url = uri("https://plugins.gradle.org/m2/")
+        }
     }
 }
 dependencyResolutionManagement {
